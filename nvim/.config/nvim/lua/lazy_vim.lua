c556bf43d490cfc4638fc4cb21315aaa8eca2b9a
eran.sbt0 <eran.sbt0@gmail.com>
2024-07-26 01:44:15 +0300

    glance rm deps

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -1568,9 +1568,6 @@ require("lazy").setup({
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
-		dependencies = {
-			"folke/which-key.nvim",
-		},
 		config = function()
 			require("gitsigns").setup({
 				on_attach = function(bufnr)
