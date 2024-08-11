ee2cebd406d4700ffdf4fdedd79c7a6fba34780a
eran.sbt0 <eran.sbt0@gmail.com>
2024-08-11 22:04:58 +0300

    remove glance, add nvim-bqf

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -1,6 +1,10 @@
 require("lazy").setup({
 	-- Editor
 	{
+		"kevinhwang91/nvim-bqf",
+		ft = "qf",
+	},
+	{
 		"nvim-lua/plenary.nvim",
 		lazy = false,
 	},
@@ -995,84 +999,6 @@ require("lazy").setup({
 				{ path = "luvit-meta/library", words = { "vim%.uv" } },
 			},
 		},
-	},
-	{
-		"dnlhc/glance.nvim",
-		lazy = false,
-		config = function()
-			local glance = require("glance")
-			local actions = glance.actions
-
-			glance.setup({
-				preview_win_opts = { -- Configure preview window options
-					cursorline = true,
-					number = true,
-					wrap = true,
-				},
-				border = {
-					enable = false, -- Show window borders. Only horizontal borders allowed
-					top_char = "―",
-					bottom_char = "―",
-				},
-				list = {
-					position = "right", -- Position of the list window 'left'|'right'
-					width = 0.33, -- 33% width relative to the active window, min 0.1, max 0.5
-				},
-				theme = { -- This feature might not work properly in nvim-0.7.2
-					enable = true, -- Will generate colors for the plugin based on your current colorscheme
-					mode = "auto", -- 'brighten'|'darken'|'auto', 'auto' will set mode based on the brightness of your colorscheme
-				},
-				mappings = {
-					list = {
-						["j"] = actions.next, -- Bring the cursor to the next item in the list
-						["k"] = actions.previous, -- Bring the cursor to the previous item in the list
-						["<Down>"] = actions.next,
-						["<Up>"] = actions.previous,
-						["<Tab>"] = actions.next_location, -- Bring the cursor to the next location skipping groups in the list
-						["<S-Tab>"] = actions.previous_location, -- Bring the cursor to the previous location skipping groups in the list
-						["<C-u>"] = actions.preview_scroll_win(5),
-						["<C-d>"] = actions.preview_scroll_win(-5),
-						["v"] = actions.jump_vsplit,
-						["s"] = actions.jump_split,
-						["t"] = actions.jump_tab,
-						["<CR>"] = actions.jump,
-						["o"] = actions.jump,
-						["l"] = actions.open_fold,
-						["h"] = actions.close_fold,
-						[";"] = actions.enter_win("preview"), -- Focus preview window
-						["q"] = actions.close,
-						["Q"] = actions.close,
-						["<Esc>"] = actions.close,
-						["<C-q>"] = actions.quickfix,
-						-- ['<Esc>'] = false -- disable a mapping
-					},
-					preview = {
-						["Q"] = actions.close,
-						["<Tab>"] = actions.next_location,
-						["<S-Tab>"] = actions.previous_location,
-						[";"] = actions.enter_win("list"), -- Focus list window
-					},
-				},
-				hooks = {},
-				folds = {
-					fold_closed = "",
-					fold_open = "",
-					folded = true, -- Automatically fold list on startup
-				},
-				indent_lines = {
-					enable = true,
-					icon = "│",
-				},
-				winbar = {
-					enable = true, -- Available strating from nvim-0.8+
-				},
-			})
-
-			vim.keymap.set("n", "gr", "<cmd>Glance references<cr>", { noremap = true, silent = true })
-			vim.keymap.set("n", "gD", "<cmd>Glance definitions<cr>", { noremap = true, silent = true })
-			vim.keymap.set("n", "gT", "<cmd>Glance type_definitions<cr>", { noremap = true, silent = true })
-			vim.keymap.set("n", "gI", "<cmd>Glance implementations<cr>", { noremap = true, silent = true })
-		end,
 	},
 	{
 		"rmagatti/goto-preview",
