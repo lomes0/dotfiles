0acf20a6deebda466a0e80f475dc5c9f379c672d
eran.sbt0 <eran.sbt0@gmail.com>
2024-09-21 23:29:04 +0300

    use render-markdown.nvim

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -765,12 +765,11 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"OXY2DEV/markview.nvim",
-		ft = "markdown",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
 	},
 	{
 		"folke/twilight.nvim",
