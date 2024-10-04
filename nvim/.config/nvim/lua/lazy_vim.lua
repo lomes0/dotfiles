a37922f5eaff28ae0aba927ce723ddbd595c2a45
eran.sbt0 <eran.sbt0@gmail.com>
2024-10-05 02:27:19 +0300

    change to aventa

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -1,8 +1,4 @@
 require("lazy").setup({
-	-- {
-	-- 	"github/copilot.vim",
-	-- 	lazy = false,
-	-- },
 	{
 		"akinsho/bufferline.nvim",
 		requires = "nvim-tree/nvim-web-devicons",
@@ -23,50 +19,191 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"olimorris/codecompanion.nvim",
+		"yetone/avante.nvim",
+		event = "VeryLazy",
 		lazy = false,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
-			"nvim-telescope/telescope.nvim", -- Optional: For using slash commands
-			{ "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves the default Neovim UI
-		},
-		config = function()
-			require("codecompanion").setup({
-				strategies = {
-					chat = {
-						adapter = "llama3",
-					},
-					inline = {
-						adapter = "llama3",
-					},
-					agent = {
-						adapter = "llama3",
-					},
-				},
-				adapters = {
-					llama3 = function()
-						return require("codecompanion.adapters").extend("ollama", {
-							name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
-							schema = {
-								model = {
-									default = "llama3:latest",
-								},
-								num_ctx = {
-									default = 2048,
-									-- default = 16384,
-								},
-								num_predict = {
-									default = -1,
-								},
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+			provider = "ollama", -- Recommend using Claude
+			auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+			vendors = {
+				---@type AvanteProvider
+				ollama = {
+					["local"] = true,
+					endpoint = "127.0.0.1:11434/v1",
+					model = "llama3",
+					parse_curl_args = function(opts, code_opts)
+						return {
+							url = opts.endpoint .. "/chat/completions",
+							headers = {
+								["Accept"] = "application/json",
+								["Content-Type"] = "application/json",
 							},
-						})
+							body = {
+								model = opts.model,
+								messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+								max_tokens = 2048,
+								stream = true,
+							},
+						}
+					end,
+					parse_response_data = function(data_stream, event_state, opts)
+						require("avante.providers").openai.parse_response(data_stream, event_state, opts)
 					end,
 				},
-			})
-		end,
+			},
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-3-5-sonnet-20240620",
+				temperature = 0,
+				max_tokens = 4096,
+			},
+			behaviour = {
+				auto_suggestions = false, -- Experimental stage
+				auto_set_highlight_group = true,
+				auto_set_keymaps = true,
+				auto_apply_diff_after_generation = false,
+				support_paste_from_clipboard = false,
+			},
+			mappings = {
+				--- @class AvanteConflictMappings
+				diff = {
+					ours = "co",
+					theirs = "ct",
+					all_theirs = "ca",
+					both = "cb",
+					cursor = "cc",
+					next = "]x",
+					prev = "[x",
+				},
+				suggestion = {
+					accept = "<M-l>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+				jump = {
+					next = "]]",
+					prev = "[[",
+				},
+				submit = {
+					normal = "<CR>",
+					insert = "<C-s>",
+				},
+				sidebar = {
+					switch_windows = "<Tab>",
+					reverse_switch_windows = "<S-Tab>",
+				},
+			},
+			hints = { enabled = true },
+			windows = {
+				---@type "right" | "left" | "top" | "bottom"
+				position = "right", -- the position of the sidebar
+				wrap = true, -- similar to vim.o.wrap
+				width = 30, -- default % based on available width
+				sidebar_header = {
+					align = "center", -- left, center, right for title
+					rounded = true,
+				},
+			},
+			highlights = {
+				---@type AvanteConflictHighlights
+				diff = {
+					current = "DiffText",
+					incoming = "DiffAdd",
+				},
+			},
+			--- @class AvanteConflictUserConfig
+			diff = {
+				autojump = true,
+				---@type string | fun(): any
+				list_opener = "copen",
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
 	},
+	-- {
+	-- 	"olimorris/codecompanion.nvim",
+	-- 	lazy = false,
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+	-- 		"nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+	-- 		{ "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves the default Neovim UI
+	-- 	},
+	-- 	config = function()
+	-- 		require("codecompanion").setup({
+	-- 			strategies = {
+	-- 				chat = {
+	-- 					adapter = "llama3",
+	-- 				},
+	-- 				inline = {
+	-- 					adapter = "llama3",
+	-- 				},
+	-- 				agent = {
+	-- 					adapter = "llama3",
+	-- 				},
+	-- 			},
+	-- 			adapters = {
+	-- 				llama3 = function()
+	-- 					return require("codecompanion.adapters").extend("ollama", {
+	-- 						name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
+	-- 						schema = {
+	-- 							model = {
+	-- 								default = "llama3:latest",
+	-- 							},
+	-- 							num_ctx = {
+	-- 								default = 2048,
+	-- 								-- default = 16384,
+	-- 							},
+	-- 							num_predict = {
+	-- 								default = -1,
+	-- 							},
+	-- 						},
+	-- 					})
+	-- 				end,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"lomes0/vim-tmux-navigator",
 		cmd = {
