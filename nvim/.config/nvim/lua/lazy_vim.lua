c052bdcf51427299f3a397b32548f131f382fec6
eran.sbt0 <eran.sbt0@gmail.com>
2024-10-06 02:40:24 +0300

    telescope extensions

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -1844,6 +1844,16 @@ require("lazy").setup({
 						-- theme = "dropdown",
 					},
 				},
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						-- the default case_mode is "smart_case"
+					},
+					undo = {},
+				},
 			})
 
 			local function git_root()
@@ -1897,6 +1907,12 @@ require("lazy").setup({
 					desc = "Telescope treesitter",
 				},
 			})
+
+			require("telescope").load_extension("fzf")
+
+			require("telescope").load_extension("undo")
+
+			require("telescope").load_extension("dap")
 		end,
 	},
 	-- {
