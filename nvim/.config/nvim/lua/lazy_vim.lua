c27f1baf4ddadd397b7bb1a6ca531ca10d8ea82e
eran.sbt0 <eran.sbt0@gmail.com>
2024-10-06 02:40:35 +0300

    move zellij comment

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -1915,18 +1915,6 @@ require("lazy").setup({
 			require("telescope").load_extension("dap")
 		end,
 	},
-	-- {
-	-- 	"swaits/zellij-nav.nvim",
-	-- 	lazy = true,
-	-- 	event = "VeryLazy",
-	-- 	keys = {
-	-- 		{ "<C-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
-	-- 		{ "<C-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-	-- 		{ "<C-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-	-- 		{ "<C-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
-	-- 	},
-	-- 	opts = {},
-	-- },
 	{
 		"mikavilpas/yazi.nvim",
 		lazy = true,
