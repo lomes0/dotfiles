7510dfd70a329ecd5a590aa385bf944cb593231b
eran.sbt0 <eran.sbt0@gmail.com>
2024-12-07 12:18:12 +0200

    add folke/snacks

--- nvim/.config/nvim/lua/lazy_vim.lua
+++ nvim/.config/nvim/lua/lazy_vim.lua
@@ -1,168 +1,208 @@
 require("lazy").setup({
-	--------------
-	-- Ai
-	--------------
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	cmd = "Copilot",
-	-- 	event = "InsertEnter",
-	-- 	config = function()
-	-- 		require("copilot").setup({
-	-- 			panel = {
-	-- 				enabled = true,
-	-- 				auto_refresh = false,
-	-- 				keymap = {
-	-- 					jump_prev = "[[",
-	-- 					jump_next = "]]",
-	-- 					accept = "<CR>",
-	-- 					refresh = "gr",
-	-- 					open = "<M-CR>",
-	-- 				},
-	-- 				layout = {
-	-- 					position = "bottom", -- | top | left | right
-	-- 					ratio = 0.4,
-	-- 				},
-	-- 			},
-	-- 			suggestion = {
-	-- 				enabled = true,
-	-- 				auto_trigger = false,
-	-- 				hide_during_completion = true,
-	-- 				debounce = 75,
-	-- 				keymap = {
-	-- 					accept = "<M-l>",
-	-- 					accept_word = false,
-	-- 					accept_line = false,
-	-- 					next = "<M-]>",
-	-- 					prev = "<M-[>",
-	-- 					dismiss = "<C-]>",
-	-- 				},
-	-- 			},
-	-- 			filetypes = {
-	-- 				yaml = false,
-	-- 				markdown = false,
-	-- 				help = false,
-	-- 				gitcommit = false,
-	-- 				gitrebase = false,
-	-- 				hgcommit = false,
-	-- 				svn = false,
-	-- 				cvs = false,
-	-- 				["."] = false,
-	-- 			},
-	-- 			copilot_node_command = "node", -- Node.js version must be > 18.x
-	-- 			server_opts_overrides = {},
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			win = {
+				enabled = true,
+				border = "rounded",
+			},
+			bigfile = {
+				enabled = true,
+				size = 1.5 * 1024 * 1024, -- 1.5MB
+			},
+			dashboard = {
+				enabled = true,
+			},
+			notifier = {
+				enabled = true,
+				timeout = 3000,
+			},
+			quickfile = {
+				enabled = true,
+			},
+			statuscolumn = {
+				enabled = true,
+			},
+			words = {
+				enabled = true,
+			},
+			styles = {
+				notification = {
+					wo = { wrap = true }, -- Wrap notifications
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>.",
+				function()
+					Snacks.scratch()
+				end,
+				desc = "Toggle Scratch Buffer",
+			},
+			{
+				"<leader>S",
+				function()
+					Snacks.scratch.select()
+				end,
+				desc = "Select Scratch Buffer",
+			},
+			{
+				"<leader>n",
+				function()
+					Snacks.notifier.show_history()
+				end,
+				desc = "Notification History",
+			},
+			{
+				"<leader>bd",
+				function()
+					Snacks.bufdelete()
+				end,
+				desc = "Delete Buffer",
+			},
+			{
+				"<leader>cR",
+				function()
+					Snacks.rename.rename_file()
+				end,
+				desc = "Rename File",
+			},
+			{
+				"<leader>gB",
+				function()
+					Snacks.gitbrowse()
+				end,
+				desc = "Git Browse",
+			},
+			{
+				"<leader>gb",
+				function()
+					Snacks.git.blame_line()
+				end,
+				desc = "Git Blame Line",
+			},
+			{
+				"<leader>gf",
+				function()
+					Snacks.lazygit.log_file()
+				end,
+				desc = "Lazygit Current File History",
+			},
+			{
+				"<leader>gg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "Lazygit",
+			},
+			{
+				"<leader>gl",
+				function()
+					Snacks.lazygit.log()
+				end,
+				desc = "Lazygit Log (cwd)",
+			},
+			{
+				"<leader>un",
+				function()
+					Snacks.notifier.hide()
+				end,
+				desc = "Dismiss All Notifications",
+			},
+			{
+				"<c-\\>",
+				function()
+					Snacks.terminal()
+				end,
+				desc = "Toggle Terminal",
+			},
+			{
+				";;",
+				function()
+					Snacks.words.jump(vim.v.count1)
+				end,
+				desc = "Next Reference",
+				mode = { "n", "t" },
+			},
+			{
+				";[",
+				function()
+					Snacks.words.jump(-vim.v.count1)
+				end,
+				desc = "Prev Reference",
+				mode = { "n", "t" },
+			},
+			{
+				"<leader>N",
+				desc = "Neovim News",
+				function()
+					Snacks.win({
+						file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+						width = 0.6,
+						height = 0.6,
+						wo = {
+							spell = false,
+							wrap = false,
+							signcolumn = "yes",
+							statuscolumn = " ",
+							conceallevel = 3,
+						},
+					})
+				end,
+			},
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					_G.bt = function()
+						Snacks.debug.backtrace()
+					end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+					-- Create some toggle mappings
+					Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+					Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+					Snacks.toggle.diagnostics():map("<leader>ud")
+					Snacks.toggle.line_number():map("<leader>ul")
+					Snacks.toggle
+						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+						:map("<leader>uc")
+					Snacks.toggle.treesitter():map("<leader>uT")
+					Snacks.toggle
+						.option("background", { off = "light", on = "dark", name = "Dark Background" })
+						:map("<leader>ub")
+					Snacks.toggle.inlay_hints():map("<leader>uh")
+				end,
+			})
+		end,
+	},
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		lazy = false,
 		version = false, -- set this if you want to always pull the latest change
 		opts = {
-			---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-			provider = "ollama", -- Recommend using Claude
-			auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-			vendors = {
-				ollama = {
-					["local"] = true,
-					endpoint = "127.0.0.1:11434/v1",
-					model = "llama3",
-					parse_curl_args = function(opts, code_opts)
-						return {
-							url = opts.endpoint .. "/chat/completions",
-							headers = {
-								["Accept"] = "application/json",
-								["Content-Type"] = "application/json",
-							},
-							body = {
-								model = opts.model,
-								messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-								max_tokens = 2048,
-								stream = true,
-							},
-						}
-					end,
-					parse_response_data = function(data_stream, event_state, opts)
-						require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-					end,
-				},
-			},
-			claude = {
-				endpoint = "https://api.anthropic.com",
-				model = "claude-3-5-sonnet-20240620",
-				temperature = 0,
-				max_tokens = 4096,
-			},
-			behaviour = {
-				auto_suggestions = false, -- Experimental stage
-				auto_set_highlight_group = true,
-				auto_set_keymaps = true,
-				auto_apply_diff_after_generation = false,
-				support_paste_from_clipboard = false,
-			},
-			mappings = {
-				--- @class AvanteConflictMappings
-				diff = {
-					ours = "co",
-					theirs = "ct",
-					all_theirs = "ca",
-					both = "cb",
-					cursor = "cc",
-					next = "]x",
-					prev = "[x",
-				},
-				suggestion = {
-					accept = "<M-l>",
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-				jump = {
-					next = "]]",
-					prev = "[[",
-				},
-				submit = {
-					normal = "<CR>",
-					insert = "<C-s>",
-				},
-				sidebar = {
-					switch_windows = "<Tab>",
-					reverse_switch_windows = "<S-Tab>",
-				},
-			},
-			hints = { enabled = true },
-			windows = {
-				---@type "right" | "left" | "top" | "bottom"
-				position = "right", -- the position of the sidebar
-				wrap = true, -- similar to vim.o.wrap
-				width = 45, -- default % based on available width
-				sidebar_header = {
-					align = "center", -- left, center, right for title
-					rounded = true,
-				},
-			},
-			highlights = {
-				diff = {
-					current = "DiffText",
-					incoming = "DiffAdd",
-				},
-			},
-			--- @class AvanteConflictUserConfig
-			diff = {
-				autojump = true,
-				---@type string | fun(): any
-				list_opener = "copen",
-			},
+			-- add any opts here
 		},
 		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 		build = "make",
 		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
 			--- The below dependencies are optional,
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
 			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 			"zbirenbaum/copilot.lua", -- for providers='copilot'
 			{
@@ -185,27 +225,163 @@ require("lazy").setup({
 			{
 				-- Make sure to set this up properly if you have lazy=true
 				"MeanderingProgrammer/render-markdown.nvim",
-				dependencies = {
-					"nvim-treesitter/nvim-treesitter",
-					"echasnovski/mini.nvim",
-				},
 				opts = {
 					file_types = { "markdown", "Avante" },
-					anti_conceal = { enabled = false },
-					win_options = {
-						concealcursor = {
-							default = vim.api.nvim_get_option_value("concealcursor", {}),
-							rendered = "nc",
-						},
-					},
 				},
 				ft = { "markdown", "Avante" },
 			},
 		},
 	},
-	--------------
-	-- Editor
-	--------------
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	lazy = false,
+	-- 	version = false, -- set this if you want to always pull the latest change
+	-- 	opts = {
+	-- 		---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+	-- 		provider = "ollama", -- Recommend using Claude
+	-- 		auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+	-- 		vendors = {
+	-- 			ollama = {
+	-- 				["local"] = true,
+	-- 				endpoint = "127.0.0.1:11434/v1",
+	-- 				model = "llama3",
+	-- 				parse_curl_args = function(opts, code_opts)
+	-- 					return {
+	-- 						url = opts.endpoint .. "/chat/completions",
+	-- 						headers = {
+	-- 							["Accept"] = "application/json",
+	-- 							["Content-Type"] = "application/json",
+	-- 						},
+	-- 						body = {
+	-- 							model = opts.model,
+	-- 							messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+	-- 							max_tokens = 2048,
+	-- 							stream = true,
+	-- 						},
+	-- 					}
+	-- 				end,
+	-- 				parse_response_data = function(data_stream, event_state, opts)
+	-- 					require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+	-- 				end,
+	-- 			},
+	-- 		},
+	-- 		claude = {
+	-- 			endpoint = "https://api.anthropic.com",
+	-- 			model = "claude-3-5-sonnet-20240620",
+	-- 			temperature = 0,
+	-- 			max_tokens = 4096,
+	-- 		},
+	-- 		behaviour = {
+	-- 			auto_suggestions = false, -- Experimental stage
+	-- 			auto_set_highlight_group = true,
+	-- 			auto_set_keymaps = true,
+	-- 			auto_apply_diff_after_generation = false,
+	-- 			support_paste_from_clipboard = false,
+	-- 		},
+	-- 		mappings = {
+	-- 			--- @class AvanteConflictMappings
+	-- 			diff = {
+	-- 				ours = "co",
+	-- 				theirs = "ct",
+	-- 				all_theirs = "ca",
+	-- 				both = "cb",
+	-- 				cursor = "cc",
+	-- 				next = "]x",
+	-- 				prev = "[x",
+	-- 			},
+	-- 			suggestion = {
+	-- 				accept = "<M-l>",
+	-- 				next = "<M-]>",
+	-- 				prev = "<M-[>",
+	-- 				dismiss = "<C-]>",
+	-- 			},
+	-- 			jump = {
+	-- 				next = "]]",
+	-- 				prev = "[[",
+	-- 			},
+	-- 			submit = {
+	-- 				normal = "<CR>",
+	-- 				insert = "<C-s>",
+	-- 			},
+	-- 			sidebar = {
+	-- 				switch_windows = "<Tab>",
+	-- 				reverse_switch_windows = "<S-Tab>",
+	-- 			},
+	-- 		},
+	-- 		hints = { enabled = true },
+	-- 		windows = {
+	-- 			---@type "right" | "left" | "top" | "bottom"
+	-- 			position = "right", -- the position of the sidebar
+	-- 			wrap = true, -- similar to vim.o.wrap
+	-- 			width = 45, -- default % based on available width
+	-- 			sidebar_header = {
+	-- 				align = "center", -- left, center, right for title
+	-- 				rounded = true,
+	-- 			},
+	-- 		},
+	-- 		highlights = {
+	-- 			diff = {
+	-- 				current = "DiffText",
+	-- 				incoming = "DiffAdd",
+	-- 			},
+	-- 		},
+	-- 		--- @class AvanteConflictUserConfig
+	-- 		diff = {
+	-- 			autojump = true,
+	-- 			---@type string | fun(): any
+	-- 			list_opener = "copen",
+	-- 		},
+	-- 	},
+	-- 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- 	build = "make",
+	-- 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		--- The below dependencies are optional,
+	-- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+	-- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+	-- 		{
+	-- 			-- support for image pasting
+	-- 			"HakonHarnes/img-clip.nvim",
+	-- 			event = "VeryLazy",
+	-- 			opts = {
+	-- 				-- recommended settings
+	-- 				default = {
+	-- 					embed_image_as_base64 = false,
+	-- 					prompt_for_file_name = false,
+	-- 					drag_and_drop = {
+	-- 						insert_mode = true,
+	-- 					},
+	-- 					-- required for Windows users
+	-- 					use_absolute_path = true,
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 		{
+	-- 			-- Make sure to set this up properly if you have lazy=true
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			dependencies = {
+	-- 				"nvim-treesitter/nvim-treesitter",
+	-- 				"echasnovski/mini.nvim",
+	-- 			},
+	-- 			opts = {
+	-- 				file_types = { "markdown", "Avante" },
+	-- 				anti_conceal = { enabled = false },
+	-- 				win_options = {
+	-- 					concealcursor = {
+	-- 						default = vim.api.nvim_get_option_value("concealcursor", {}),
+	-- 						rendered = "nc",
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 			ft = { "markdown", "Avante" },
+	-- 		},
+	-- 	},
+	-- },
 	{
 		"akinsho/bufferline.nvim",
 		requires = "nvim-tree/nvim-web-devicons",
