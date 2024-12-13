3cd8210f68f1e255b823261423f5774dbf4b4e61
eran.sbt0 <eran.sbt0@gmail.com>
2024-12-13 22:12:04 +0200

    noice --> editor.lua

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -545,4 +545,141 @@ return {
 			})
 		end,
 	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("noice").setup({
+				debug = false,
+				cmdline = {
+					enabled = true, -- enables the Noice cmdline UI
+					view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+					opts = {}, -- global options for the cmdline. See section on views
+					format = {
+						cmdline = { pattern = "^:", icon = "", lang = "vim" },
+						search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+						search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+						filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+						lua = {
+							pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" },
+							icon = "",
+							lang = "lua",
+						},
+						help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+						input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+						-- lua = false, -- to disable a format, set to `false`
+					},
+				},
+				lsp = {
+					progress = {
+						enabled = false,
+					},
+					override = {
+						-- override the default lsp markdown formatter with Noice
+						["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+						-- override the lsp markdown formatter with Noice
+						["vim.lsp.util.stylize_markdown"] = false,
+						-- override cmp documentation with Noice (needs the other options to work)
+						["cmp.entry.get_documentation"] = false,
+					},
+					hover = {
+						enabled = true,
+						silent = false, -- set to true to not show a message if hover is not available
+						view = nil, -- when nil, use defaults from documentation
+						---@type NoiceViewOptions
+						opts = {}, -- merged with defaults from documentation
+					},
+					signature = {
+						enabled = true,
+						auto_open = {
+							enabled = true,
+							trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+							luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+							throttle = 50, -- Debounce lsp signature help request by 50ms
+						},
+						view = nil, -- when nil, use defaults from documentation
+						---@type NoiceViewOptions
+						opts = {}, -- merged with defaults from documentation
+					},
+					message = {
+						-- Messages shown by lsp servers
+						enabled = true,
+						view = "notify",
+						opts = {},
+					},
+					-- defaults for hover and signature help
+					documentation = {
+						view = "hover",
+						---@type NoiceViewOptions
+						opts = {
+							lang = "markdown",
+							replace = true,
+							render = "plain",
+							format = { "{message}" },
+							win_options = { concealcursor = "n", conceallevel = 3 },
+						},
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = false, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = true, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+				messages = {
+					-- notify
+					-- split
+					-- vsplit
+					-- popup
+					-- mini
+					-- cmdline
+					-- cmdline_popup
+					-- cmdline_output
+					-- messages
+					-- confirm
+					-- hover
+					-- popupmenu
+					enabled = true, -- enables the Noice messages UI
+					view = "notify", -- default view for messages
+					view_error = "notify", -- view for errors
+					view_warn = "notify", -- view for warnings
+					view_history = "messages", -- view for :messages
+					view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+				},
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							cmdline = ":w",
+						},
+						opts = { skip = true },
+					},
+					{
+						filter = {
+							event = "msg_show",
+							find = "AutoSave",
+						},
+						opts = { skip = true },
+					},
+					{
+						filter = {
+							event = "msg_show",
+							find = "Hunk",
+						},
+						opts = { skip = true },
+					},
+				},
+				popupmenu = {
+					enabled = true, -- enables the Noice popupmenu UI
+					backend = "nui", -- backend to use to show regular cmdline completions
+					kind_icons = {}, -- set to `false` to disable icons
+				},
+			})
+		end,
+	},
 }
