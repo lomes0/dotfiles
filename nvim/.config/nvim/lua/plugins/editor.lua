aa838d6c57e05308cddc3dfc5379a35b2c2f4ee9
eran.sbt0 <eran.sbt0@gmail.com>
2024-12-19 23:07:24 +0200

    Snacks keymaps descs

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -296,91 +296,91 @@ return {
 				function()
 					Snacks.scratch()
 				end,
-				desc = "Toggle Scratch Buffer",
+				desc = "Snacks Toggle Scratch Buffer",
 			},
 			{
 				"<leader>S",
 				function()
 					Snacks.scratch.select()
 				end,
-				desc = "Select Scratch Buffer",
+				desc = "Snacks Select Scratch Buffer",
 			},
 			{
 				"<leader>n",
 				function()
 					Snacks.notifier.show_history()
 				end,
-				desc = "Notification History",
+				desc = "Snacks Notification History",
 			},
 			{
 				"<leader>bd",
 				function()
 					Snacks.bufdelete()
 				end,
-				desc = "Delete Buffer",
+				desc = "Snacks Delete Buffer",
 			},
 			{
 				"<leader>cR",
 				function()
 					Snacks.rename.rename_file()
 				end,
-				desc = "Rename File",
+				desc = "Snacks Rename File",
 			},
 			{
 				"<leader>gB",
 				function()
 					Snacks.gitbrowse()
 				end,
-				desc = "Git Browse",
+				desc = "Snacks Git Browse",
 			},
 			{
 				"<leader>gb",
 				function()
 					Snacks.git.blame_line()
 				end,
-				desc = "Git Blame Line",
+				desc = "Snacks Git Blame Line",
 			},
 			{
 				"<leader>gf",
 				function()
 					Snacks.lazygit.log_file()
 				end,
-				desc = "Lazygit Current File History",
+				desc = "Snacks Lazygit Current File History",
 			},
 			{
 				"<leader>gg",
 				function()
 					Snacks.lazygit()
 				end,
-				desc = "Lazygit",
+				desc = "Snacks Lazygit",
 			},
 			{
 				"<leader>gl",
 				function()
 					Snacks.lazygit.log()
 				end,
-				desc = "Lazygit Log (cwd)",
+				desc = "Snacks Lazygit Log (cwd)",
 			},
 			{
 				"<leader>un",
 				function()
 					Snacks.notifier.hide()
 				end,
-				desc = "Dismiss All Notifications",
+				desc = "Snacks Dismiss All Notifications",
 			},
 			{
 				"<c-\\>",
 				function()
 					Snacks.terminal()
 				end,
-				desc = "Toggle Terminal",
+				desc = "Snacks Toggle Terminal",
 			},
 			{
 				";;",
 				function()
 					Snacks.words.jump(vim.v.count1)
 				end,
-				desc = "Next Reference",
+				desc = "Snacks Next Reference",
 				mode = { "n", "t" },
 			},
 			{
@@ -388,12 +388,12 @@ return {
 				function()
 					Snacks.words.jump(-vim.v.count1)
 				end,
-				desc = "Prev Reference",
+				desc = "Snacks Prev Reference",
 				mode = { "n", "t" },
 			},
 			{
 				"<leader>N",
-				desc = "Neovim News",
+				desc = "Snacks Neovim News",
 				function()
 					Snacks.win({
 						file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
@@ -424,19 +424,19 @@ return {
 					vim.print = _G.dd -- Override print to use snacks for `:=` command
 
 					-- Create some toggle mappings
-					Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-					Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-					Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-					Snacks.toggle.diagnostics():map("<leader>ud")
-					Snacks.toggle.line_number():map("<leader>ul")
+					Snacks.toggle.option("spell", { name = "Snacks Spelling" }):map("<leader>us")
+					Snacks.toggle.option("wrap", { name = "Snacks Wrap" }):map("<leader>uw")
+					Snacks.toggle.option("relativenumber", { name = "Snacks Relative Number" }):map("<leader>uL")
+					Snacks.toggle.diagnostics({ name = "Snacks Diagnostics" }):map("<leader>ud")
+					Snacks.toggle.line_number({ name = "" }):map("<leader>ul")
 					Snacks.toggle
 						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
 						:map("<leader>uc")
-					Snacks.toggle.treesitter():map("<leader>uT")
+					Snacks.toggle.treesitter({ name = "Snacks Treesitter" }):map("<leader>uT")
 					Snacks.toggle
 						.option("background", { off = "light", on = "dark", name = "Dark Background" })
 						:map("<leader>ub")
-					Snacks.toggle.inlay_hints():map("<leader>uh")
+					Snacks.toggle.inlay_hints({ name = "Snacks Inlay Hints" }):map("<leader>uh")
 				end,
 			})
 		end,
