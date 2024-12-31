7f9489a730c3a2c3ceaf079d902649e9752dc507
eran.sbt0 <eran.sbt0@gmail.com>
2025-01-01 01:28:26 +0200

    disable statuscolumn for terminals

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -443,6 +443,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	callback = function()
 		vim.opt_local.number = false
 		vim.opt_local.relativenumber = false
+		vim.opt_local.statuscolumn = ""
 	end,
 })
 
