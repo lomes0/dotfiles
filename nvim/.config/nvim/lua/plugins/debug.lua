89a95c03e735c11d156161ca2dbe9d8fc6272e2d
eran.sbt0 <eran.sbt0@gmail.com>
2025-07-28 00:44:35 +0300

    nvim dap

--- nvim/.config/nvim/lua/plugins/debug.lua
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -13,29 +13,25 @@ return {
 
 			dap.adapters.lldb = {
 				type = "executable",
-				command = "/u/eransa/.vscode-server/extensions/vadimcn.vscode-lldb-1.11.4/adapter/codelldb",
+				command = "lldb-dap",
+				name = "lldb",
 			}
 
 			dap.configurations.cpp = {
 				{
-					name = "fwk",
-					type = "codelldb",
-					request = "attach",
-					pid = 11076,
-					initCommands = {
-						"platform select remote-linux",
-						"platform connect connect://172.23.53.48:3444",
-						"process handle SIGWINCH -p true -n false -s false",
-						"settings append target.source-map /local_ckp/src/cpis/jess_main/release.dynamic.x64 /u/eransa/code/cpas_profiling/mt/cpis",
-						"settings append target.source-map /local_ckp/src/kiss_apps/jess_main/debug.dynamic.x32 /u/eransa/code/cpas_profiling/mt/kiss_apps",
-						"settings append target.source-map /local_ckp/src/streaming/jess_main/debug.dynamic /u/eransa/code/cpas_profiling/mt/streaming",
-						"settings append target.source-map /local_ckp/src/mux/jess_main/debug.dynamic /u/eransa/code/cpas_profiling/mt/mux",
-						"settings append target.source-map /local_ckp/src/ws/jess_main/debug.dynamic /u/eransa/code/cpas_profiling/mt/ws",
-						"settings append target.source-map /local_ckp/src/fw1/jess_main/debug.dynamic.x32 /u/eransa/code/cpas_profiling/mt/fw1",
-					},
+					name = "Launch",
+					type = "lldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+					runInTerminal = false,
 				},
 			}
-
+			dap.configurations.c = dap.configurations.cpp
 			dap.configurations.rust = dap.configurations.cpp
 
 			-------
