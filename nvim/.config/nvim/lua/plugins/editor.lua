3ec757f42ad5fbefc35977329f64e5e05ff9a345
eran.sbt0 <eran.sbt0@gmail.com>
2025-10-02 13:29:21 +0300

    lualine

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -928,57 +928,57 @@ return {
 			})
 		end,
 	},
-	-- {
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	event = "VeryLazy",
-	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- 	config = function()
-	-- 		require("lualine").setup({
-	-- 			options = {
-	-- 				icons_enabled = true,
-	-- 				theme = "auto",
-	-- 				component_separators = { left = "", right = "" },
-	-- 				section_separators = { left = "", right = "" },
-	-- 				disabled_filetypes = {
-	-- 					statusline = {},
-	-- 					winbar = {},
-	-- 				},
-	-- 				ignore_focus = {},
-	-- 				always_divide_middle = true,
-	-- 				globalstatus = false,
-	-- 				refresh = {
-	-- 					statusline = 1000,
-	-- 					tabline = 1000,
-	-- 					winbar = 1000,
-	-- 				},
-	-- 			},
-	-- 			sections = {
-	-- 				lualine_a = { "mode" },
-	-- 				lualine_b = { "branch", "diff", "diagnostics" },
-	-- 				lualine_c = {
-	-- 					{
-	-- 						"filename",
-	-- 						path = 2,
-	-- 					},
-	-- 					{
-	-- 						"_G.clipboard_icon",
-	-- 					},
-	-- 				},
-	-- 				lualine_e = {},
-	-- 				lualine_x = { "encoding", "fileformat", "filetype" },
-	-- 				lualine_y = { "progress" },
-	-- 				lualine_z = { "location" },
-	-- 			},
-	-- 			inactive_sections = {
-	-- 				lualine_a = {},
-	-- 			},
-	-- 			tabline = {},
-	-- 			winbar = {},
-	-- 			inactive_winbar = {},
-	-- 			extensions = {},
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "auto",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					},
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						{
+							"filename",
+							path = 2,
+						},
+						{
+							"_G.clipboard_icon",
+						},
+					},
+					lualine_e = {},
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {},
+			})
+		end,
+	},
 	{
 		"nvim-telescope/telescope.nvim",
 		event = "VeryLazy",
