0aff35362ed90c3452cf26671e7357cbf0ca5199
eran.sbt0 <eran.sbt0@gmail.com>
2025-10-06 21:53:20 +0300

    init.lua

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -13,7 +13,7 @@ local opts = {
 	{ "tabstop", 4 },
 	{ "softtabstop", 4 },
 	{ "shiftwidth", 4 },
-	{ "scrolloff", 8 },
+	{ "scrolloff", 4 },
 	{ "updatetime", 50 },
 	{ "timeoutlen", 300 },
 	{ "conceallevel", 2 },
@@ -657,6 +657,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
 		vim.keymap.set("n", "gca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+
+		vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
 	end,
 })
 
