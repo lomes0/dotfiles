9322383ef23318550c158afa9b329b3d4bb6e38d
eran.sbt0 <eran.sbt0@gmail.com>
2025-10-06 22:25:19 +0300

    rm mdx

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -61,12 +61,6 @@ return {
 					end
 				end,
 			})
-			vim.filetype.add({
-				extension = {
-					mdx = "mdx",
-				},
-			})
-			vim.treesitter.language.register("markdown", "mdx")
 		end,
 	},
 	{
